7c81a4081d70d7cdda0e76a6d9aa2f1a69d2004b
LEKS <ilhanchanel20002@gmail.com>
2024-01-06 18:14:42 +0300

    Added metamask-sdk and it's provider

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
+import { MetaMaskProvider } from '@metamask/sdk-react';
 
-import {store} from './store' 
+import { store } from './store'
 
 
 export default function Providers(props: React.HTMLAttributes<HTMLDivElement>) {
+    // client window is used to avoid "window is not defined" error
+    const [clientWindow, setClientWindow] = useState<Window|undefined>()
+
+    useEffect(() => setClientWindow(window), [])
+
     return <Provider store={store}>
-        {props.children}
+        <MetaMaskProvider debug={false} sdkOptions={{
+            dappMetadata: {
+                name: "Ursas Planet",
+                url: clientWindow?.location.host,
+            }
+        }}>
+            {props.children}
+        </MetaMaskProvider>
     </Provider>
 }
