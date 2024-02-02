cbf16b964e1bf579d84c842798408b135869de03
LEKS <ilhanchanel20002@gmail.com>
2024-02-02 18:51:20 +0300

    Issue 68

--- src/components/Header/SectionRight.tsx
+++ src/components/Header/SectionRight.tsx
@@ -90,7 +90,7 @@ export default function SectionRight() {
                 animated
                 color='dark'
                 iconStart={
-                    !!isConnected() &&
+                    !isConnected() &&
                     <FaWallet />
                 }
                 iconEnd={
