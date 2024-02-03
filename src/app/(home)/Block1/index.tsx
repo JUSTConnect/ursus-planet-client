3bfa70fec5e4cf73c8d81819f47db101e1304dc4
LEKS <ilhanchanel20002@gmail.com>
2024-02-03 18:27:31 +0300

    Issue 129

--- src/app/(home)/Block1/index.tsx
+++ src/app/(home)/Block1/index.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import Image from 'next/image'
+import { useDispatch } from 'react-redux';
 
 import { SiOpenlayers } from "react-icons/si";
 import { FaPlay } from "react-icons/fa6";
 
 import css from './index.module.scss'
-import iconPlayer from './img/icon-player.png'
-import iconProject from './img/icon-project.png'
 import figureUrsa from './img/figure.svg'
 import bgSpace from './img/bg-space.jpg'
 import bgRelief from './img/relief.svg'
 import bgLForest from './img/forest-left.svg'
 import bgRForest from './img/forest-right.svg'
 
+import { setModalWalletConnect } from '@/features/modal/modalSlice';
 import { useMetaMask } from '@/hooks/useMetamask';
 import Box from '@/components/core/Box'
 import Container from '@/components/core/Container'
@@ -28,6 +28,7 @@ import BottomSection from './BottomSection'
 
 
 export default function Block1() {
+    const dispatch = useDispatch()
     const {isConnected} = useMetaMask()
 
     return <Box className={css.wrapper}>
@@ -56,6 +57,7 @@ export default function Block1() {
             </div>
             <Stack fullWidth justifyCenter>
                 <Button
+                    onClick={() => !isConnected() && dispatch(setModalWalletConnect(true))}
                     size='lg'
                     color={isConnected() ? 'primary' : 'dark'}
                     iconStart={<FaPlay />}
@@ -64,6 +66,7 @@ export default function Block1() {
                     PLAYER
                 </Button>
                 <Button
+                    onClick={() => !isConnected() && dispatch(setModalWalletConnect(true))}
                     size='lg'
                     color="dark"
                     iconStart={<SiOpenlayers />}
