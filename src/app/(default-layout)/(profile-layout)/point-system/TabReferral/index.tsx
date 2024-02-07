d50faeb41ea00de9072ec3d20b2ef3c5c970b17b
LEKS <ilhanchanel20002@gmail.com>
2024-02-08 00:55:03 +0300

    Feature: Issue 40

--- src/app/(default-layout)/(profile-layout)/point-system/TabReferral/index.tsx
+++ src/app/(default-layout)/(profile-layout)/point-system/TabReferral/index.tsx
@@ -1,25 +1,37 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image';
+
+import { Table } from '@radix-ui/themes';
 
 import { FaInfoCircle } from "react-icons/fa";
 import { FaLink } from "react-icons/fa6";
+import { FaCopy } from "react-icons/fa6";
 
-import { CardBody, CardFooter } from "@/components/core/Card"
+import { CardBody } from "@/components/core/Card"
 import Container from "@/components/core/Container"
 import { ICardTab } from '@/components/CardTabs'
+import Box from '@/components/core/Box';
+import { Input } from '@/components/core/Input';
+import Button from '@/components/core/Button';
+
 import CardTabs from '../../CardTabs'
 import Card from '../../Card'
 import CardHead from '../../CardHead'
 import Cards from '../../Cards'
 
+import css from './index.module.scss'
+import figure from './img/figure.svg'
+import messageBottom from './img/message-bottom.svg'
 
-type TabName = 'projects' | 'platform'
+
+type TabName = 'code' | 'info'
 
 
 export default function TabProfile() {
 
-    const [activeTab, setActiveTab] = useState<TabName>('projects')
+    const [activeTab, setActiveTab] = useState<TabName>('code')
 
     const mobileTabs: ICardTab[] = [
         {
@@ -42,29 +54,92 @@ export default function TabProfile() {
         />
         <Cards>
             <Card
-                active={activeTab === 'projects'}
+                active={activeTab === 'code'}
             >
-                <div>
-                    <CardHead
-                        title='Referral code'
-                        icon={<FaLink />}   
-                    />
-                    <CardBody>
-
-                    </CardBody>
-                </div>
+                <CardHead
+                    title='Referral code'
+                    icon={<FaLink />}
+                />
+                <CardBody className={css.codeBody}>
+                    <Box className={css.codeTop}>
+                        <Box className={css.figure}>
+                            <Image
+                                src={figure}
+                                alt='figure'
+                            />
+                        </Box>
+                        <Box className={css.codeContent}>
+                            <Box className={css.codeMessage}>
+                                <Box mb={1}>
+                                    Enter refferal code  - <span className={css.points}>900 points</span>
+                                </Box>
+                                <Box>
+                                    Refferal player  - <span className={css.points}>900 points</span>
+                                </Box>
+                                <Image
+                                    className={css.codeMessageBottom}
+                                    src={messageBottom}
+                                    alt='figure'
+                                />
+                            </Box>
+                            <Box className={css.codeDescription}>
+                                We recommend an image of at least 300x300. Gift work too. Max 5 mb.
+                            </Box>
+                            <Input
+                                placeholder='Enter refferal code'
+                            />
+                        </Box>
+                    </Box>
+                    <Box className={css.codeBottom}>
+                        <div className={css.codeBottomCode}>
+                            Your referal code : 4t43g554ehgb54reregergerg
+                        </div>
+                        <Box>
+                            <Button className={css.codeBottomButton} color='white'>Copy link</Button>
+                        </Box>
+                        <FaCopy className={css.codeBottomButtonMobile} />
+                    </Box>
+                </CardBody>
             </Card>
             <Card
-                active={activeTab === 'platform'}
+                active={activeTab === 'info'}
             >
-                <div>
-                    <CardHead
-                        title='Referral info'
-                        icon={<FaInfoCircle/>}
-                    />
-                    <CardBody>
-                    </CardBody>
-                </div>
+                <CardHead
+                    title='Referral info'
+                    icon={<FaInfoCircle />}
+                />
+                <CardBody className={css.codeBody}>
+                    <Box>
+                        <Box mb={1}>
+                            Total referrals : <span className={css.infoValue}>30</span>
+                        </Box>
+                        <Box mb={1}>
+                            Quote of referrals : <span className={css.infoValue}>30</span>
+                        </Box>
+
+                        <Table.Root className={css.infoTable}>
+                            <Table.Body>
+                                {
+                                    Array.from(Array(5)).map((_, index)=>
+                                        <Table.Row>
+                                            <Table.RowHeaderCell>1. Username</Table.RowHeaderCell>
+                                            <Table.Cell>0x0ad6a1b701edde68520538d502c16900a1ee73f5</Table.Cell>
+                                            <Table.Cell>101</Table.Cell>
+                                        </Table.Row>
+                                    )
+                                }
+                            </Table.Body>
+                        </Table.Root>
+                    </Box>
+                    <Box className={css.infoNote}>
+                        <Box mb={1} className={css.infoNoteTitle}>
+                            Note:
+                        </Box>
+                        <Box>
+                            Your daily invitation limit is equal to the number of your current referrals. Exceeding the daily invitation limit will invalidate the code for the rest of the day.
+                        </Box>
+                    </Box>
+                </CardBody>
             </Card>
         </Cards>
     </Container>
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Theme } from '@radix-ui/themes';
 import { Provider } from "react-redux"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { MetaMaskContextProvider } from "@/hooks/useMetamask"
