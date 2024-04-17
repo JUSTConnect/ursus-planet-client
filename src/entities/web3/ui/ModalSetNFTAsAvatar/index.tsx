1b6d84c1cffec58535fc8cbddce65509f7c05b28
Sukuna <nAsad1385@gmail.com>
2024-04-18 01:03:39 +0300

    Fix account state 4

--- src/entities/web3/ui/ModalSetNFTAsAvatar/index.tsx
+++ src/entities/web3/ui/ModalSetNFTAsAvatar/index.tsx
@@ -54,7 +54,7 @@ export default function ModalSetNFTAsAvatar(props: Omit<React.ComponentProps<typ
                     <Text align='center'>choose NFT as your profile picture</Text>
                     <Card.Root>
                         <Card.Body>
-                            <Grid height="2" columns='4' gap='3'>
+                            <Grid className="h-80" columns='4' gap='3'>
                                 {
                                     NFTs && NFTs.map((item, index) =>
                                         <Box
