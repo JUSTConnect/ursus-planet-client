3fa13363b54665f2e481f5b8d45d85d4e318fbff
Sukuna <nAsad1385@gmail.com>
2024-04-18 00:50:01 +0300

    hm

--- src/entities/web3/ui/ModalSetNFTAsAvatar/index.tsx
+++ src/entities/web3/ui/ModalSetNFTAsAvatar/index.tsx
@@ -54,7 +54,7 @@ export default function ModalSetNFTAsAvatar(props: Omit<React.ComponentProps<typ
                     <Text align='center'>choose NFT as your profile picture</Text>
                     <Card.Root>
                         <Card.Body>
-                            <Grid height="auto" columns='4' gap='3'>
+                            <Grid height="min-content" columns='4' gap='3'>
                                 {
                                     NFTs && NFTs.map((item, index) =>
                                         <Box
